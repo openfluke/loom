a525b4be20528af5f5f595c43b11d234e5547978
openfluke-dev <openfluke@hotmail.com>
2026-03-11 17:45:03 +1100

    added cnn1 tiling

--- poly/cnn1.go
+++ poly/cnn1.go
@@ -8,6 +8,9 @@ package poly
 
 // CNN1ForwardPolymorphic performs a forward pass through a 1D convolutional layer.
 func CNN1ForwardPolymorphic[T Numeric](layer *VolumetricLayer, input *Tensor[T]) (preAct, postAct *Tensor[T]) {
+	if layer.UseTiling && layer.TileSize > 0 {
+		return CNN1ForwardTiled(layer, input)
+	}
 	batchSize := input.Shape[0]
 	seqLen, inC := layer.InputHeight, layer.InputChannels
 	outLen, filters := layer.OutputHeight, layer.Filters
@@ -206,6 +209,9 @@ func CNN1ForwardPolymorphic[T Numeric](layer *VolumetricLayer, input *Tensor[T])
 
 // CNN1BackwardPolymorphic calculates gradients for a 1D convolutional layer.
 func CNN1BackwardPolymorphic[T Numeric](layer *VolumetricLayer, gradOutput, input, preAct *Tensor[T]) (gradInput, gradWeights *Tensor[T]) {
+	if layer.UseTiling && layer.TileSize > 0 {
+		return CNN1BackwardTiled(layer, gradOutput, input, preAct)
+	}
 	batchSize := input.Shape[0]
 	seqLen, inC := layer.InputHeight, layer.InputChannels
 	outLen, filters := layer.OutputHeight, layer.Filters
@@ -377,6 +383,128 @@ func CNN1BackwardPolymorphic[T Numeric](layer *VolumetricLayer, gradOutput, inpu
 
 							gradInput.Data[inIdx] += T(gOut * float32(wData[kWIdx]))
 							gradWeights.Data[kWIdx] += T(gOut * float32(input.Data[inIdx]))
+						}
+					}
+				}
+			}
+		}
+	}
+	return gradInput, gradWeights
+}
+
+// CNN1ForwardTiled implements a loop-blocked forward pass for CNN1.
+func CNN1ForwardTiled[T Numeric](layer *VolumetricLayer, input *Tensor[T]) (preAct, postAct *Tensor[T]) {
+	batchSize := input.Shape[0]
+	seqLen, inC := layer.InputHeight, layer.InputChannels
+	outLen, filters := layer.OutputHeight, layer.Filters
+	kSize, stride, padding := layer.KernelSize, layer.Stride, layer.Padding
+	tileSize := layer.TileSize
+	if tileSize <= 0 {
+		tileSize = 16
+	}
+
+	preAct = NewTensor[T](batchSize, filters, outLen)
+	postAct = NewTensor[T](batchSize, filters, outLen)
+
+	weights := layer.WeightStore.GetActive(layer.DType)
+	if weights == nil {
+		weights = layer.WeightStore.Master
+	}
+	wData := CastWeights[T](weights)
+
+	for b := 0; b < batchSize; b++ {
+		// Output tiles (Spatial Blocking)
+		for oTile := 0; oTile < outLen; oTile += tileSize {
+			oEnd := oTile + tileSize
+			if oEnd > outLen {
+				oEnd = outLen
+			}
+
+			// Filter tiles
+			for fTile := 0; fTile < filters; fTile += tileSize {
+				fEnd := fTile + tileSize
+				if fEnd > filters {
+					fEnd = filters
+				}
+
+				for o := oTile; o < oEnd; o++ {
+					for f := fTile; f < fEnd; f++ {
+						var sum float32
+						for ic := 0; ic < inC; ic++ {
+							for k := 0; k < kSize; k++ {
+								inPos := o*stride + k - padding
+								if inPos >= 0 && inPos < seqLen {
+									inIdx := b*inC*seqLen + ic*seqLen + inPos
+									kWIdx := f*inC*kSize + ic*kSize + k
+									sum += float32(input.Data[inIdx]) * float32(wData[kWIdx])
+								}
+							}
+						}
+						outIdx := b*filters*outLen + f*outLen + o
+						preAct.Data[outIdx] = T(sum)
+					}
+				}
+			}
+		}
+		// Final Activation pass (Cache-friendly)
+		outBBase := b * filters * outLen
+		for i := 0; i < filters*outLen; i++ {
+			idx := outBBase + i
+			postAct.Data[idx] = Activate(preAct.Data[idx], layer.Activation)
+		}
+	}
+	return preAct, postAct
+}
+
+// CNN1BackwardTiled implements a loop-blocked backward pass for CNN1.
+func CNN1BackwardTiled[T Numeric](layer *VolumetricLayer, gradOutput, input, preAct *Tensor[T]) (gradInput, gradWeights *Tensor[T]) {
+	batchSize := input.Shape[0]
+	seqLen, inC := layer.InputHeight, layer.InputChannels
+	outLen, filters := layer.OutputHeight, layer.Filters
+	kSize, stride, padding := layer.KernelSize, layer.Stride, layer.Padding
+	tileSize := layer.TileSize
+	if tileSize <= 0 {
+		tileSize = 16
+	}
+
+	gradInput = NewTensor[T](batchSize, inC, seqLen)
+	gradWeights = NewTensor[T](filters, inC, kSize)
+
+	weights := layer.WeightStore.GetActive(layer.DType)
+	if weights == nil {
+		weights = layer.WeightStore.Master
+	}
+	wData := CastWeights[T](weights)
+
+	for b := 0; b < batchSize; b++ {
+		for fTile := 0; fTile < filters; fTile += tileSize {
+			fEnd := fTile + tileSize
+			if fEnd > filters {
+				fEnd = filters
+			}
+
+			for oTile := 0; oTile < outLen; oTile += tileSize {
+				oEnd := oTile + tileSize
+				if oEnd > outLen {
+					oEnd = outLen
+				}
+
+				for f := fTile; f < fEnd; f++ {
+					for o := oTile; o < oEnd; o++ {
+						outIdx := b*filters*outLen + f*outLen + o
+						gOut := float32(gradOutput.Data[outIdx]) * float32(ActivateDerivative(preAct.Data[outIdx], layer.Activation))
+
+						for ic := 0; ic < inC; ic++ {
+							for k := 0; k < kSize; k++ {
+								inPos := o*stride + k - padding
+								if inPos >= 0 && inPos < seqLen {
+									inIdx := b*inC*seqLen + ic*seqLen + inPos
+									kWIdx := f*inC*kSize + ic*kSize + k
+
+									gradInput.Data[inIdx] += T(gOut * float32(wData[kWIdx]))
+									gradWeights.Data[kWIdx] += T(gOut * float32(input.Data[inIdx]))
+								}
+							}
 						}
 					}
 				}
