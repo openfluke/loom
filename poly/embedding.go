14ff1b444993a99a5ebc4df0fe4256426df22d32
openfluke-dev <openfluke@hotmail.com>
2026-03-11 17:57:30 +1100

    added embedding tiling

--- poly/embedding.go
+++ poly/embedding.go
@@ -2,6 +2,9 @@ package poly
 
 // EmbeddingForwardPolymorphic performs an embedding lookup across any numerical type.
 func EmbeddingForwardPolymorphic[T Numeric](layer *VolumetricLayer, input *Tensor[T]) (preAct, postAct *Tensor[T]) {
+	if layer.UseTiling && layer.TileSize > 0 {
+		return EmbeddingForwardTiled(layer, input)
+	}
 	vocabSize := layer.VocabSize
 	embeddingDim := layer.EmbeddingDim
 	seqLen := len(input.Data)
@@ -12,7 +15,9 @@ func EmbeddingForwardPolymorphic[T Numeric](layer *VolumetricLayer, input *Tenso
 	postAct = NewTensor[T](outShape...)
 
 	weights := layer.WeightStore.GetActive(layer.DType)
-	if weights == nil { weights = layer.WeightStore.Master }
+	if weights == nil {
+		weights = layer.WeightStore.Master
+	}
 
 	switch layer.DType {
 	case DTypeFloat64:
@@ -109,7 +114,9 @@ func EmbeddingForwardPolymorphic[T Numeric](layer *VolumetricLayer, input *Tenso
 
 	// Universal fallback
 	scaleW := layer.WeightStore.Scale
-	if scaleW == 0 { scaleW = 1.0 }
+	if scaleW == 0 {
+		scaleW = 1.0
+	}
 	wData := CastWeights[float32](weights)
 
 	for i := 0; i < seqLen; i++ {
@@ -129,12 +136,15 @@ func EmbeddingForwardPolymorphic[T Numeric](layer *VolumetricLayer, input *Tenso
 
 // EmbeddingBackwardPolymorphic computes gradients for embedding lookup.
 func EmbeddingBackwardPolymorphic[T Numeric](layer *VolumetricLayer, gradOutput, input, preAct *Tensor[T]) (gradInput, gradWeights *Tensor[T]) {
+	if layer.UseTiling && layer.TileSize > 0 {
+		return EmbeddingBackwardTiled(layer, gradOutput, input, preAct)
+	}
 	vocabSize := layer.VocabSize
 	embeddingDim := layer.EmbeddingDim
 	seqLen := len(input.Data)
 
 	gradInput = NewTensor[T](input.Shape...)
-	gradWeights = NewTensor[T](vocabSize * embeddingDim)
+	gradWeights = NewTensor[T](vocabSize, embeddingDim)
 
 	// Direct lookup backward
 	for i := 0; i < seqLen; i++ {
@@ -144,6 +154,110 @@ func EmbeddingBackwardPolymorphic[T Numeric](layer *VolumetricLayer, gradOutput,
 		}
 		for j := 0; j < embeddingDim; j++ {
 			gradWeights.Data[tokenID*embeddingDim+j] += gradOutput.Data[i*embeddingDim+j]
+		}
+	}
+
+	return gradInput, gradWeights
+}
+
+// EmbeddingForwardTiled implements a loop-blocked embedding lookup for cache efficiency.
+func EmbeddingForwardTiled[T Numeric](layer *VolumetricLayer, input *Tensor[T]) (preAct, postAct *Tensor[T]) {
+	vocabSize := layer.VocabSize
+	embeddingDim := layer.EmbeddingDim
+	seqLen := len(input.Data)
+	tileSize := layer.TileSize
+	if tileSize <= 0 {
+		tileSize = 32
+	}
+
+	outShape := append([]int{}, input.Shape...)
+	outShape = append(outShape, embeddingDim)
+	preAct = NewTensor[T](outShape...)
+	postAct = NewTensor[T](outShape...)
+
+	weights := layer.WeightStore.GetActive(layer.DType)
+	if weights == nil {
+		weights = layer.WeightStore.Master
+	}
+	wData := CastWeights[float32](weights)
+	scaleW := layer.WeightStore.Scale
+	if scaleW == 0 {
+		scaleW = 1.0
+	}
+
+	// Double-blocked lookup
+	for iTile := 0; iTile < seqLen; iTile += tileSize {
+		iEnd := iTile + tileSize
+		if iEnd > seqLen {
+			iEnd = seqLen
+		}
+
+		for jTile := 0; jTile < embeddingDim; jTile += tileSize {
+			jEnd := jTile + tileSize
+			if jEnd > embeddingDim {
+				jEnd = embeddingDim
+			}
+
+			// Core tile
+			for i := iTile; i < iEnd; i++ {
+				tokenID := int(input.Data[i])
+				if tokenID < 0 || tokenID >= vocabSize {
+					continue
+				}
+				rowBase := tokenID * embeddingDim
+				outBase := i * embeddingDim
+				for j := jTile; j < jEnd; j++ {
+					val := SimulatePrecision(wData[rowBase+j], layer.DType, scaleW)
+					preAct.Data[outBase+j] = T(val)
+				}
+			}
+		}
+	}
+
+	// Final Activation pass
+	for i := 0; i < len(preAct.Data); i++ {
+		postAct.Data[i] = Activate(preAct.Data[i], layer.Activation)
+	}
+
+	return preAct, postAct
+}
+
+// EmbeddingBackwardTiled implements a loop-blocked gradient calculation for embeddings.
+func EmbeddingBackwardTiled[T Numeric](layer *VolumetricLayer, gradOutput, input, preAct *Tensor[T]) (gradInput, gradWeights *Tensor[T]) {
+	vocabSize := layer.VocabSize
+	embeddingDim := layer.EmbeddingDim
+	seqLen := len(input.Data)
+	tileSize := layer.TileSize
+	if tileSize <= 0 {
+		tileSize = 32
+	}
+
+	gradInput = NewTensor[T](input.Shape...)
+	gradWeights = NewTensor[T](vocabSize, embeddingDim)
+
+	for iTile := 0; iTile < seqLen; iTile += tileSize {
+		iEnd := iTile + tileSize
+		if iEnd > seqLen {
+			iEnd = seqLen
+		}
+
+		for jTile := 0; jTile < embeddingDim; jTile += tileSize {
+			jEnd := jTile + tileSize
+			if jEnd > embeddingDim {
+				jEnd = embeddingDim
+			}
+
+			for i := iTile; i < iEnd; i++ {
+				tokenID := int(input.Data[i])
+				if tokenID < 0 || tokenID >= vocabSize {
+					continue
+				}
+				rowBase := tokenID * embeddingDim
+				outBase := i * embeddingDim
+				for j := jTile; j < jEnd; j++ {
+					gradWeights.Data[rowBase+j] += gradOutput.Data[outBase+j]
+				}
+			}
 		}
 	}
 
