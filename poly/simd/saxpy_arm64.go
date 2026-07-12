6c22e01a3d0bd227543eb1625b9daed55dc10fde
openfluke-dev <openfluke@hotmail.com>
2026-07-12 14:44:24 +1000

    fixed arm and 1.1 to maybe 2ish simd

--- poly/simd/saxpy_arm64.go
+++ poly/simd/saxpy_arm64.go
@@ -2,12 +2,11 @@
 
 package simd
 
-//go:noescape
-func saxpyF32AccF64Neon(acc *float64, alpha float64, x *float32, n int)
+import "unsafe"
 
 func saxpyF32AccF64Simd(acc *float64, alpha float64, x *float32, n int) {
 	if n <= 0 {
 		return
 	}
-	saxpyF32AccF64Neon(acc, alpha, x, n)
+	saxpyF32AccF64Go(unsafe.Slice(acc, n), alpha, unsafe.Slice(x, n), n)
 }
--- poly/simd/saxpy_neon_arm64.s
+++ poly/simd/saxpy_neon_arm64.s
@@ -3,25 +3,30 @@
 #include "textflag.h"
 
 // func saxpyF32AccF64Neon(acc *float64, alpha float64, x *float32, n int)
+//
+// acc[i] += alpha * float64(x[i]). Four-wide NEON loop mirrors dot_neon_arm64.s
+// widen+FMA pattern; scalar tail matches saxpy_avx2_amd64.s operation order.
 TEXT ·saxpyF32AccF64Neon(SB), NOSPLIT, $0-32
 	MOVD acc+0(FP), R0
-	MOVSD alpha+8(FP), F0
+	FMOVD alpha+8(FP), F0
 	MOVD x+16(FP), R1
 	MOVD n+24(FP), R2
 
 	CMP $4, R2
 	BLT tail
 
-	FDUP V31.D2, F0
+	FMOVD alpha+8(FP), F31
+	WORD $0x4e0807ff // DUP V31.2D, V31.D[0]
 
 loop4:
 	VLD1.P 16(R1), [V0.S4]
-	VLD1.P 32(R0), [V1.D2, V2.D2]
-	WORD $0x0E617800 // VFCVTL  V3.D2, V0.S2
-	WORD $0x4E617801 // VFCVTL2 V4.D2, V0.S4
-	VFMLA V3.D2, V31.D2, V1.D2
-	VFMLA V4.D2, V31.D2, V2.D2
-	VST1.P [V1.D2, V2.D2], 32(R0)
+	VLD1 (R0), [V1.D2, V2.D2]
+	WORD $0x0E617810 // VFCVTL  V0.S2, V16.D2
+	WORD $0x4E617811 // VFCVTL2 V0.S4, V17.D2
+	VFMLA V16.D2, V31.D2, V1.D2
+	VFMLA V17.D2, V31.D2, V2.D2
+	VST1 [V1.D2, V2.D2], (R0)
+	ADD $32, R0
 	SUB $4, R2
 	CMP $4, R2
 	BGE loop4
@@ -29,12 +34,12 @@ loop4:
 tail:
 	CBZ R2, done
 tail1:
-	LDRSW (R1), R3
-	FCVTSD F2, R3
-	FMULD F2, F0, F2
-	LDRD (R0), F3
-	FADDD F2, F3, F2
-	STRD F2, (R0)
+	FMOVS (R1), F4
+	FCVTSD F4, F2
+	FMULD F0, F2
+	FMOVD (R0), F3
+	FADDD F3, F2
+	FMOVD F2, (R0)
 	ADD $4, R1
 	ADD $8, R0
 	SUB $1, R2
